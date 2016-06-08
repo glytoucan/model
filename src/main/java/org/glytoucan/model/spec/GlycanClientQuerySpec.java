618ac370cde97af8932722771afd14bb44d28d18
aoki <aokinobu@gmail.com>
2016-06-08 12:04:32 +0900

    new spec

--- src/main/java/org/glytoucan/model/spec/GlycanClientQuerySpec.java
+++ src/main/java/org/glytoucan/model/spec/GlycanClientQuerySpec.java
@@ -6,6 +6,12 @@ import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.util.Map;
 
+/**
+ * 
+ * Query REST specification.  Environment variables such as server names or ports is left to the implementation. 
+ * 
+ * @author aoki
+ */
 public interface GlycanClientQuerySpec {
 
 	public static final String USERNAME = "USERNAME";
@@ -18,19 +24,17 @@ public interface GlycanClientQuerySpec {
 	public static final String REGISTRATION = "REGISTRATION";
 	public static final String CONTEXT_PATH = "CONTEXT_PATH";
 	public static final String SEQUENCE = "SEQUENCE";
+  public static final String ID = "ID";
 	public static final String LIST_CMD = "/list";
 	public static final String PAYLOAD = "payload";
 	public static final String LIMIT = "limit";
 	public static final String OFFSET = "offset";
+  public static final String IMAGE_FORMAT = "IMAGE_FORMAT";
+  public static final String IMAGE_NOTATION = "IMAGE_NOTATION";
+  public static final String IMAGE_STYLE = "IMAGE_STYLE";
 		
     public Map<String, Object> getListStructures(Map<String, Object> gmap);
 
-    String getImage(String hostname, String sequence)
+    String getImage(Map<String, Object> data)
         throws IOException, NoSuchAlgorithmException, KeyStoreException, KeyManagementException;
-
-    String getImage(String hostname, String sequence, Map<String, Object> data)
-        throws IOException, NoSuchAlgorithmException, KeyStoreException, KeyManagementException;
-
-    String getRegisteredImage(String hostname, String accessionNumber, String imageFormat, String notation,
-        String style) throws IOException;
 }
