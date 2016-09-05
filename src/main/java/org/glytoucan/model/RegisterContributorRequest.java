0d791d6601b49449254053f86884fd1bc6407d8e
aoki <aokinobu@gmail.com>
2016-09-05 20:13:08 +0900

    registercontributorrequest

---
+++ src/main/java/org/glytoucan/model/RegisterContributorRequest.java
@@ -0,0 +1,15 @@
+package org.glytoucan.model;
+
+public class RegisterContributorRequest {
+
+	String name;
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+}
